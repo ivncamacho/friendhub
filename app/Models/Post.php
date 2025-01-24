c968adae320ab0cf8c9e9ce33ad102b302b664f3
LAPTOP-FIK1QKO4\ivan <1891569@alu.murciaeduca.es>
2025-01-24 19:46:10 +0100

    Añadimos migracion y modelo Comments y creamos relaciones

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Post extends Model
 {
@@ -23,5 +24,10 @@ class Post extends Model
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function comments(): hasMany
+    {
+        return $this->hasMany(Comment::class);
     }
 }
