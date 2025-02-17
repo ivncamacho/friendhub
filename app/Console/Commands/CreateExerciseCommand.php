e7ed893af1931c500f08a720c4675f9d4b146472
LAPTOP-FIK1QKO4\ivan <1891569@alu.murciaeduca.es>
2025-02-18 00:05:40 +0100

    Creado comando para crear ejercicio con previamente autenticados en el mismo comando

---
+++ app/Console/Commands/CreateExerciseCommand.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Exercise;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Auth;
+
+class CreateExerciseCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'exercise:create {email} {password} {title} {description?} {media?} {youtube_video_id?}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Create an exercise';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+
+        $email = $this->argument('email');
+        $password = $this->argument('password');
+
+
+        if (Auth::attempt(['email' => $email, 'password' => $password])) {
+            $this->info("Loggued as {$email}");
+
+            $title = $this->argument('title');
+            $description = $this->argument('description') ;
+            $media = $this->argument('media');
+            $youtube_video_id = $this->argument('youtube_video_id');
+
+
+            $exercise = Exercise::create([
+                'title' => $title,
+                'description' => $description,
+                'media' => $media,
+                 'youtube_video_id'=> $youtube_video_id,
+            ]);
+
+            $this->info("Exercise '{$exercise->title}' created succesfully.");
+        } else {
+            $this->error('Incorrect credentials.');
+        }
+    }
+}
